f58f92a3d42dfecc16be65c6cf6086039200a92a
huang-me <amos0107@gmail.com>
2020-09-23 23:59:08 +0800

    Add family_class into feature and try adding age
    Replacing nan with 25 didn't help predict the survival rate

--- parchSurvivalRalation.py
+++ parchSurvivalRalation.py
@@ -9,9 +9,16 @@ train_data = pd.read_csv('train.csv')
 parch = train_data.Parch
 
 survive = train_data.Survived
-train_data['family'] = train_data.Parch + train_data.SibSp
+train_data['age_class4'] = pd.qcut(train_data.Age, 4)
+train_data['age_class5'] = pd.qcut(train_data.Age, 5)
+train_data['age_class6'] = pd.qcut(train_data.Age, 6)
 
-sns.countplot(x='Parch', hue='Survived', data=train_data)
+#sns.countplot(x='age_class', hue='Survived', data=train_data)
 #pd.crosstab(train_data.Parch, train_data.Survived).plot(kind='bar', stacked=True)
-sns.factorplot(x='family', y='Survived', data=train_data)
+
+#train_data.loc[train_data.Age <]
+
+sns.factorplot(x='age_class4', y='Survived', data=train_data)
+sns.factorplot(x='age_class5', y='Survived', data=train_data)
+sns.factorplot(x='age_class6', y='Survived', data=train_data)
 plt.show()
--- titanic.py
+++ titanic.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+#from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
-from statistics import stdev
+from sklearn.preprocessing import LabelEncoder
+#from sklearn import tree
 
 train_data = pd.read_csv('train.csv')
 
@@ -12,14 +15,25 @@ sexdic = {'male': 1,'female': 2}
 train_data.Sex = [sexdic[item] for item in train_data.Sex]
 #print(train_data)
 
+# sex
 sex = np.array(train_data.Sex).reshape(-1, 1)
+# pclass
 pclass = np.array(train_data.Pclass).reshape(-1, 1)
-parch = np.array(train_data.Parch).reshape(-1, 1)
+# family class
 train_data['family_size'] = train_data.SibSp + train_data.Parch
 train_data.loc[train_data['family_size'] == 0, 'family_cut'] = 0
-train_data.loc[(train_data['family_size'] >= 1) & (train_data['family_size'] < 5), 'family_cut'] = 2
-print(train_data.family_cut)
-feature = np.concatenate((sex, pclass, parch), axis=1)
+train_data.loc[(train_data['family_size'] >= 1) & (train_data['family_size'] < 5), 'family_cut'] = 1
+train_data.loc[(train_data['family_size'] >= 4) & (train_data['family_size'] < 7), 'family_cut'] = 2
+train_data.loc[train_data['family_size'] >= 7, 'family_cut'] = 3
+family_cut = np.array(train_data.family_cut).reshape(-1, 1)
+# age class
+label = LabelEncoder()
+train_data.Age[np.isnan(train_data.Age)] = 25
+train_data['age_class'] = pd.qcut(train_data.Age, 5)
+train_data['age_class_code'] = label.fit_transform(train_data.age_class)
+age_class = np.array(train_data.age_class_code).reshape(-1, 1)
+
+feature = np.concatenate((sex, pclass, family_cut, age_class), axis=1)
 # print(feature)
 
 survived = np.array(train_data.Survived).reshape(-1,1)
@@ -36,7 +50,20 @@ test_data.Sex = [sexdic[item] for item in test_data.Sex]
 sex = np.array(test_data.Sex).reshape(-1, 1)
 pclass = np.array(test_data.Pclass).reshape(-1, 1)
 parch = np.array(test_data.Parch).reshape(-1, 1)
-feature = np.concatenate((sex, pclass, parch), axis=1)
+
+test_data['family_size'] = test_data.SibSp + test_data.Parch
+test_data.loc[test_data['family_size'] == 0, 'family_cut'] = 0
+test_data.loc[(test_data['family_size'] >= 1) & (test_data['family_size'] < 5), 'family_cut'] = 1
+test_data.loc[(test_data['family_size'] >= 4) & (test_data['family_size'] < 7), 'family_cut'] = 2
+test_data.loc[test_data['family_size'] >= 7, 'family_cut'] = 3
+family_cut = np.array(test_data.family_cut).reshape(-1, 1)
+
+test_data.Age[np.isnan(test_data.Age)] = 25
+test_data['age_class'] = pd.qcut(test_data.Age, 5)
+test_data['age_class_code'] = label.fit_transform(test_data.age_class)
+age_class = np.array(test_data.age_class_code).reshape(-1, 1)
+
+feature = np.concatenate((sex, pclass, family_cut, age_class), axis=1)
 print(feature.shape)
 predict = model.predict(feature)
 
