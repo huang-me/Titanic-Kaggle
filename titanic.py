fed5d6fbbaff829cd0851cc82e5d8ad867002598
huang-me <amos0107@gmail.com>
2020-09-23 12:12:43 +0800

    Add .gitignore and add pclass into feature
    Got correctness score 0.77511

--- titanic.py
+++ titanic.py
@@ -6,9 +6,15 @@ from sklearn import tree
 train_data = pd.read_csv('train.csv')
 
 # set feature and survived data
-train_data["Sex"] = train_data["Sex"].str.replace("female", "0")
-train_data["Sex"] = train_data["Sex"].str.replace("male", "1")
-feature = np.array(train_data.Sex).reshape(-1,1)
+# creating a dict file
+sexdic = {'male': 1,'female': 2}
+train_data.Sex = [sexdic[item] for item in train_data.Sex]
+#print(train_data)
+
+sex = np.array(train_data.Sex).reshape(-1, 1)
+pclass = np.array(train_data.Pclass).reshape(-1, 1)
+feature = np.concatenate((sex, pclass), axis=1)
+#print(feature)
 survived = np.array(train_data.Survived).reshape(-1,1)
 
 # initialize the model
@@ -18,7 +24,15 @@ model = model.fit(feature, survived)
 # get test file
 test_data = pd.read_csv('test.csv')
 
-test_data["Sex"] = test_data["Sex"].str.replace("female", "0")
-test_data["Sex"] = test_data["Sex"].str.replace("male", "1")
-feature = np.array(test_data.Sex).reshape(-1,1)
-print(model.predict(feature))
+# predict test file
+test_data.Sex = [sexdic[item] for item in test_data.Sex]
+sex = np.array(test_data.Sex).reshape(-1, 1)
+pclass = np.array(test_data.Pclass).reshape(-1, 1)
+feature = np.concatenate((sex, pclass), axis=1)
+print(feature.shape)
+predict = model.predict(feature)
+
+# output the result
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived':predict})
+output.to_csv('submission.csv', index=False)
+print('submission save successfully')
